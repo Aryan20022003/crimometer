56ad11eccfcf19b54872265416604b3cc932dac6
Aryan20022003 <sharmakumaranil24121968@gmail.com>
2023-04-28 23:23:43 +0530

    text box is added

--- src/Advisory.jsx
+++ src/Advisory.jsx
@@ -2,13 +2,28 @@ import React, { useState } from "react";
 import Record from "./List";
 import generateTravelAdvisory from './getAPI';
 import ResponseCard from "./ResponseCard";
-
+const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 const Advisiory = function (props) {
     const [generatedText, setGeneratedText] = useState('');
-    // Fetch the travel advisory and update the intermediate generated text
-    const { location, month, crimes: crimeData } = props.data;
-    console.log("hello world");
-    console.log(location, month, crimeData,);
+    const { location, month, crimes: crimeData, totalCrimes } = props.data;
+
+    if (totalCrimes === 0) {
+        return <h3 className="text-5xl text-gray-400 pb-3 text-center pt-5 pb-5">{`No crime during ${monthNames[month - 1]}`}</h3>;
+    }
+
     const fetchTravelAdvisory = async () => {
         console.log("gpt button");
         try {
@@ -18,21 +33,19 @@ const Advisiory = function (props) {
             setGeneratedText('AI is OFFLINE');
         }
     };
-    return <section className="p-4 ">
-        <h3 className="text-5xl text-gray-400 pb-3">Total Number of Crime : {props.data.totalCrimes}</h3>
-        <section className="mt-1 mx-auto flex flex-col lg:flex-row lg:gap-10 lg:justify-center" >
-            <div className="lg:mx-2">
-                <Record data={props.data} />
-                <button className="btn btn-active btn-accent ml-2" onClick={fetchTravelAdvisory}>Generate AI advisory</button>
-            </div>
-            {/* <div className="p-2 w-1/3 h- bg-white">
-                <h3 className="">AI Generated circular</h3>
-                <p>{generatedText}</p>
-            </div> */}
-            <ResponseCard data={generatedText} />
-        </section>
-    </section>
 
+    return (
+        <section className="p-4 ">
+            <h3 className="text-5xl text-gray-400 pb-3 text-center">Total Number of Crime : {totalCrimes}</h3>
+            <section className="mt-1 mx-auto flex flex-col lg:flex-row lg:gap-10 lg:justify-center" >
+                <div className="lg:mx-2 centerCustom">
+                    <Record data={props.data} />
+                    <button className="btn btn-success btn-accent" onClick={fetchTravelAdvisory}>Generate AI advisory</button>
+                </div>
+                {generatedText ? <ResponseCard data={generatedText} /> : null}
+            </section>
+        </section>
+    );
 };
 
 export default Advisiory;
--- src/ResponseCard.jsx
+++ src/ResponseCard.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './response.css';
+
 const ResponseCard = (props) => {
-    return <div className="card w-full lg:w-2/4 h-4/5 bg-base-100 shadow-xl overflow-x-scroll overflow-y-scroll responsecard">
-        <div className="card-body">
-            <h2 className="card-title">AI generated Advisory</h2>
-            <p>{props.data}</p>
-            <div className="card-actions justify-end">
-                <button className="btn btn-primary">Copy</button>
+    const [isCopied, setIsCopied] = useState(false);
+
+    const handleCopy = () => {
+        const textToCopy = document.querySelector('.card-body p').textContent;
+        navigator.clipboard.writeText(textToCopy)
+            .then(() => {
+                setIsCopied(true);
+                setTimeout(() => setIsCopied(false), 3000);
+            });
+    };
+
+    const copyButtonText = isCopied ? 'Copied to Clipboard' : 'Copy';
+
+    const buttonClassName = isCopied ? 'btn btn-success w-40 h-4' : 'btn btn-primary w-28 h-4';
+
+    return (
+        <div className="card w-full lg:w-2/4 h-4/5 bg-base-100 shadow-xl overflow-x-scroll overflow-y-scroll responsecard">
+            <div className="card-body break-words">
+                <h2 className="card-title">AI generated Advisory</h2>
+                <p>{props.data}</p>
+                <div className="card-actions justify-end">
+                    <button className={buttonClassName} onClick={handleCopy}>
+                        {copyButtonText}
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-}
+    );
+};
+
 export default ResponseCard;
