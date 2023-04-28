3558ee856e00c471a7b02554f753c44d3251f0b6
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-04-28 16:05:59 +0530

    add chatgpt api
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

---
+++ src/getAPI.js
@@ -0,0 +1,53 @@
+const generateTravelAdvisory = async (
+  location,
+  month,
+  crimeData,
+  onProgress
+) => {
+  const prompt = `Generate a travel advisory for ${location} during the month of ${month}. The crime data for this period is as follows: ${crimeData}. Provide a concise and informative summary of the safety risks and guidelines for travelers.`;
+
+  const requestBody = {
+    prompt: prompt,
+    max_tokens: 150,
+    n: 1,
+    stop: null,
+    temperature: 0.7,
+  };
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer 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`,
+    },
+    body: JSON.stringify(requestBody),
+  };
+
+  try {
+    const response = await fetch(
+      'https://api.openai.com/v1/engines/davinci-codex/completions',
+      requestOptions
+    );
+
+    if (response.ok) {
+      const reader = response.body.getReader();
+      let text = '';
+
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+
+        text += new TextDecoder().decode(value);
+        onProgress(text);
+      }
+
+      const data = JSON.parse(text);
+      return data.choices[0].text.trim();
+    } else {
+      throw new Error('Error generating travel advisory');
+    }
+  } catch (error) {
+    console.error('Error generating travel advisory:', error);
+    return null;
+  }
+};
