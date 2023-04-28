f574658f24a830056532287bed43c1609a519aec
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-04-28 23:48:13 +0530

    fix wrap in generated text box

--- src/Advisory.jsx
+++ src/Advisory.jsx
@@ -1,51 +1,64 @@
 import React, { useState } from 'react';
 import Record from './List';
 import generateTravelAdvisory from './getAPI';
-import ResponseCard from "./ResponseCard";
+import ResponseCard from './ResponseCard';
 const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 const Advisiory = function (props) {
-    const [generatedText, setGeneratedText] = useState('');
-    const { location, month, crimes: crimeData, totalCrimes } = props.data;
+  const [generatedText, setGeneratedText] = useState('');
+  const { location, month, crimes: crimeData, totalCrimes } = props.data;
 
-    if (totalCrimes === 0) {
-        return <h3 className="text-5xl text-gray-400 pb-3 text-center pt-5 pb-5">{`No crime during ${monthNames[month - 1]}`}</h3>;
-    }
-
-    const fetchTravelAdvisory = async () => {
-        console.log("gpt button");
-        try {
-            const text = await generateTravelAdvisory(location, month, crimeData);
-            setGeneratedText(text);
-        } catch (error) {
-            setGeneratedText('AI is OFFLINE');
-        }
-    };
-
+  if (totalCrimes === 0) {
     return (
-        <section className="p-4 ">
-            <h3 className="text-5xl text-gray-400 pb-3 text-center">Total Number of Crime : {totalCrimes}</h3>
-            <section className="mt-1 mx-auto flex flex-col lg:flex-row lg:gap-10 lg:justify-center" >
-                <div className="lg:mx-2 centerCustom">
-                    <Record data={props.data} />
-                    <button className="btn btn-success btn-accent" onClick={fetchTravelAdvisory}>Generate AI advisory</button>
-                </div>
-                {generatedText ? <ResponseCard data={generatedText} /> : null}
-            </section>
-        </section>
+      <h3 className="text-5xl text-gray-400 pb-3 text-center pt-5 pb-5">{`No crime during ${
+        monthNames[month - 1]
+      }`}</h3>
     );
+  }
+
+  const fetchTravelAdvisory = async () => {
+    console.log('gpt button');
+    try {
+      const text = await generateTravelAdvisory(location, month, crimeData);
+      setGeneratedText(text);
+    } catch (error) {
+      setGeneratedText('AI is OFFLINE');
+    }
+  };
+
+  return (
+    <section className="p-4 ">
+      <h3 className="text-5xl text-gray-400 pb-3 text-center">
+        Total Number of Crime : {totalCrimes}
+      </h3>
+      <section className="mt-1 mx-auto flex flex-col lg:flex-row lg:gap-10 lg:justify-center">
+        <div className="lg:mx-2 flex flex-col justify-center align-middle">
+          <Record data={props.data} />
+          <div className="flex justify-center align-middle">
+            <button
+              className="btn btn-success btn-accent"
+              onClick={fetchTravelAdvisory}
+            >
+              Generate AI advisory
+            </button>
+          </div>
+        </div>
+        {generatedText ? <ResponseCard data={generatedText} /> : null}
+      </section>
+    </section>
+  );
 };
 
 export default Advisiory;
--- src/ResponseCard.jsx
+++ src/ResponseCard.jsx
@@ -22,7 +22,7 @@ const ResponseCard = (props) => {
     <div className="card w-full lg:w-2/4 h-4/5 bg-base-100 shadow-xl overflow-x-scroll overflow-y-scroll responsecard">
       <div className="card-body break-words">
         <h2 className="card-title">AI generated Advisory</h2>
-        <p>{props.data}</p>
+        <p style={{ whiteSpace: 'pre-wrap' }}>{props.data}</p>
         <div className="card-actions justify-end">
           <button className={buttonClassName} onClick={handleCopy}>
             {copyButtonText}
