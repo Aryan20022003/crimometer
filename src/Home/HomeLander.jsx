68d39578189d0e801c6f226167a2588c02ea1d5d
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-04-29 12:49:24 +0530

    fix text in homelander

--- src/Home/HomeLander.jsx
+++ src/Home/HomeLander.jsx
@@ -1,20 +1,29 @@
-import React from "react";
-import LottieAnimation from "./Animation.jsx";
-import "./HomeLander.css"
+import React from 'react';
+import LottieAnimation from './Animation.jsx';
+import './HomeLander.css';
+
 const Home = () => {
-    return <section className="flex flex-col-reverse gap-2 md:flex-row  w-11/12 mx-auto mt-4 py-11">
-        <div className="content md:w-1/2">
-            <h1 className="title">
-                Keeping You Safe: The Power of Predictive Policing
-            </h1>
-            <p className="discription pt-4 text-lg">
-                "At Crimo Meter, our commitment is to empower communities with the tools they need to stay safe and prevent crime. By leveraging cutting-edge technology and innovative solutions, we're taking a proactive approach to crime prevention by issuing timely advisories to residents, visitors, and businesses in high-risk areas. Our mission is to create a safer world for everyone, and we invite you to join us in making this vision a reality."
-            </p>
-        </div>
-        <div className="Animation">
-            <LottieAnimation />
-        </div>
+  return (
+    <section className="flex flex-col-reverse gap-2 md:flex-row  w-11/12 mx-auto mt-4 py-11">
+      <div className="content md:w-1/2">
+        <h1 className="title">
+          Keeping You Safe: The Power of Predictive Policing
+        </h1>
+        <blockquote className="quote pt-4 text-lg">
+          <p>
+            At Crimometer, we equip communities with vital tools to bolster
+            safety and deter crime. Utilizing advanced technology, we
+            proactively deliver timely advisories to those in high-risk areas.
+            Our goal is a safer world for all, and we invite you to join us in
+            realizing this vision.
+          </p>
+        </blockquote>
+      </div>
+      <div className="Animation">
+        <LottieAnimation />
+      </div>
     </section>
-}
+  );
+};
 
 export default Home;
